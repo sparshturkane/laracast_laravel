a991aa598776af9a7fd50cf872ac22898f21c32e
sparshturkane <rajaturkane99@gmail.com>
2017-07-27 16:00:01 +0530

    layout structure completed

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    @include('layouts/head')
+    <body>
+        @include('layouts/navbar')
+
+        @yield('contents')
+    </body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/master')
+
+@section('contents')
+    <h1>Home page</h1>
+@endsection
